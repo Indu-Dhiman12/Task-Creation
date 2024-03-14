78c6f379d81710cac73fed25c7a1baabe7c0652c
Indu-Dhiman12 <137861961+Indu-Dhiman12@users.noreply.github.com>
2024-03-14 10:42:54 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+"use client"
+import { redirect, useRouter } from "next/navigation";
+import { useEffect } from "react";
+import LoginPage from "./auth/Registration/page";
+import Dashboard from "./dashBoard/page";
+
+
+export default function Home() {
+  const router = useRouter();
+
+  const token = typeof window !== 'undefined' && window.localStorage.getItem('token')
+
+
+  useEffect(() => {
+    if (!token) {
+      router.push('/auth/login');
+    }
+  }, []);
+
+  return (
+    <>
+      {token ? (
+        <>
+          <Dashboard />
+        </>
+      ) : (<>
+        <LoginPage />
+      </>)}
+    </>
+  );
+}
