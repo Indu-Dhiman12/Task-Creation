66e359211fdfae4a4b042db48b9b0b6ca7bc148d
Indu-Dhiman12 <137861961+Indu-Dhiman12@users.noreply.github.com>
2024-03-14 10:31:11 +0530

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,15 @@ import Dashboard from "./dashBoard/page";
 export default function Home() {
   const router = useRouter();
 
-  const token = localStorage.getItem('token')
+  const token = window !== undefined && window.localStorage.getItem('token')
 
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
-    console.log("abc");
     if (!token) {
       router.push('/auth/login');
     }
   }, []);
+
   return (
     <>
       {token ? (
